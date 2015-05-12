4d55541aaee6e5c48713b516dcbcc4d5f5a504a2
Marcel Großmann <marcel.grossmann@uni-bamberg.de>
2015-05-12 16:49:52 +0200

    Initial Commit

---
+++ softether/runner.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+if [ ! -d "/var/log/vpnserver/security_log" ]; then
+  mkdir -p /var/log/vpnserver/security_log
+fi
+
+if [ ! -d "/var/log/vpnserver/packet_log" ]; then
+  mkdir -p /var/log/vpnserver/packet_log
+fi
+
+if [ ! -d "/var/log/vpnserver/server_log" ]; then
+  mkdir -p /var/log/vpnserver/server_log
+fi
+
+ln -s /var/log/vpnserver/*_log /usr/local/vpnserver/
+
+exec /usr/local/vpnserver/vpnserver execsvc
+
+exit $?
