5f19853a59337f1463eb22fa261a7b34558d4b05
Marcel Grossmann <marcel@roupi.mg>
2015-12-10 18:00:19 +0100

    letsencrypt dockerbuild with git clone

--- django/docker-entrypoint.sh
+++ django/docker-entrypoint.sh
@@ -7,16 +7,31 @@ touch /srv/logs/gunicorn.log
 touch /srv/logs/access.log
 tail -n 0 -f /srv/logs/*.log &
 
-# Start Gunicorn processes
-echo Starting Gunicorn.
-exec gunicorn bootcamp.wsgi:application \
-	--name bootcamp \
-	--bind 0.0.0.0:8000 \
-	--workers 3 \
-	--keyfile $GRUNT_KEY \
-	--certfile $GRUNT_CERT \
-	--do-handshake-on-connect
-	--log-level=info \
-	--log-file=/srv/logs/gunicorn.log \
-	--access-logfile=/srv/logs/access.log \
-	"$@"
+if [ -f "$CERT/cert.pem" ] && [ -f "$CERT/key.pem" ]; then
+
+	# Start Gunicorn processes
+	echo Starting HTTPS Gunicorn.
+	exec gunicorn bootcamp.wsgi:application \
+		--name bootcamp \
+		--bind 0.0.0.0:8000 \
+		--workers 3 \
+		--keyfile $CERT/key.pem \
+		--certfile $CERT/cert.pem \
+		--ssl-version 3 \
+		--do-handshake-on-connect \
+		--log-level=info \
+		--log-file=/srv/logs/gunicorn.log \
+		--access-logfile=/srv/logs/access.log \
+		"$@"
+else
+	echo Starting HTTP Gunicorn.
+
+	exec gunicorn bootcamp.wsgi:application \
+		--name bootcamp \
+		--bind 0.0.0.0:8000 \
+		--workers 3 \
+		--log-level=info \
+		--log-file=/srv/logs/gunicorn.log \
+		--access-logfile=/srv/logs/access.log \
+		"$@"
+fi
--- letsencrypt/docker-entrypoint.sh
+++ letsencrypt/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
-/opt/letsencrypt/venv/bin/letsencrypt certonly --agree-tos --email $DNS_MAIL --domains $DNS_ENTRY --standalone
+source ./venv/bin/activate
+letsencrypt certonly --agree-tos --email $DNS_MAIL --domains $DNS_ENTRY --standalone --renew-by-default
 
 FIRST_ENTRY=$(cut -d ',' -f 1 <<< $DNS_ENTRY)
 CRT_PATH=$DNS_CERT/live/$FIRST_ENTRY
@@ -10,8 +10,8 @@ echo "Checking $CRT_PATH"
 
 if [ -d $CRT_PATH ]; then
 
-	ln -sf $CRT_PATH/fullchain.pem $DNS_CERT/cert.pem
-	ln -sf $CRT_PATH/privkey.pem $DNS_CERT/key.pem
+	cp -f $CRT_PATH/fullchain.pem $DNS_CERT/cert.pem
+	cp -f $CRT_PATH/privkey.pem $DNS_CERT/key.pem
 
 	cat $CRT_PATH/fullchain.pem $CRT_PATH/privkey.pem > $DNS_CERT/haproxy.pem
 fi
